01deaa94a68c23cc71d4cecec3165b9501357d05
Renaud Pirson <renaud.pirson@5thfloor.be>
2022-09-16 11:53:28 +0200

    backend sprint 1

--- pages/api/model/user.ts
+++ pages/api/model/user.ts
@@ -1,5 +1,13 @@
-export default class User{
+export default class User {
 
-    constructor(private id: string,  canVote = true) {
+    constructor(
+        private id: string, 
+        info: UserInfo, 
+        canVote = true) {
     }
+}
+
+export interface UserInfo {
+    name: string;
+    color: string;
 }
