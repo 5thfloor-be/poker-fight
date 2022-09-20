85f7fcc163f99d772c6c5245d20125c1b7a496c4
michaeldupuy <contact@michaeldupuy.fr>
2022-09-20 08:31:31 +0200

    Commit Orga

--- pages/versus/index.tsx
+++ pages/versus/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import styles from "../../styles/Home.module.css";
-import Card from "../Card";
+import Card from "../../components/Card";
 
 const Versus: NextPage = () => {
   const [widthScreen, setWidthScreen] = useState(0);
