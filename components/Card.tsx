428653784b67671e82e13d78112e7d935c9c5dcc
Esthefany Astete <esthefany.astete@5thfloor.be>
2022-09-20 17:11:15 +0200

    voting room

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,6 +2,7 @@ import styles from '../styles/Card.module.css';
 import Image from 'next/image';
 import { MdAccountCircle } from 'react-icons/md';
 import { Badge, OverlayTrigger, Popover } from 'react-bootstrap';
+import {GrFormClose} from "react-icons/Gr";
 
 export interface CardProps {
     value?: number;
@@ -19,59 +20,70 @@ export interface BadgeConfig {
     position?: string | null;
 }
 
-
-const Card = ({value, canClose, color, selected = false, name = null, badgeConfig = {}}: CardProps) => {
+const Card = ({ value, canClose, color, selected = false,name = null, badgeConfig = {} }: CardProps) => {
     const popover = (
-            <Popover id="popover-basic">
-                {badgeConfig?.popupTitle && <Popover.Header as="h3">{badgeConfig?.popupTitle}</Popover.Header>}
-                <Popover.Body>
-                    {badgeConfig?.popupText}
-                </Popover.Body>
-            </Popover>
+        <Popover id="popover-basic">
+            {badgeConfig?.popupTitle && <Popover.Header as="h3">{badgeConfig?.popupTitle}</Popover.Header>}
+            <Popover.Body>
+                {badgeConfig?.popupText}
+            </Popover.Body>
+        </Popover>
     )
     return (
-        <div
-            className={!selected ? `card ${styles.customCard}` : `card ${styles.customCard} ${styles.customCardSelected}`}>
-            <div>
-                <Image alt="card" src="/images/favicon.png" width={50} height={50}/>
-            </div>
-            <div className="text-center">
-                {value && <span className={`fw-bold text-success ${styles.valueCard}`}>{value}</span>}
-                {color && <MdAccountCircle color={color} size={60}/>}
-            </div>
-            <div className={styles.logoBottom}>
-                <Image alt="card" src="/images/favicon.png" width={50} height={50}/>
-            </div>
-            {
-                name && <p className="position-absolute top-100 start-100 translate-middle fw-bold pe-5 pt-3">{name}</p>
-            }
-            {
-                badgeConfig && <OverlayTrigger trigger="click" placement={badgeConfig?.position} overlay={popover}>
-                        <Badge bg={'danger'} pill={true}
-                               className={ badgeConfig.position == "left" ? "position-absolute top-0 start-0 translate-middle" : "position-absolute top-0 start-100 translate-middle"}>{badgeConfig.badgeText}</Badge>
-                </OverlayTrigger>
-            }
+        <div>
+            <div
+                className={!selected ? `card ${styles.customCard}` : `card ${styles.customCard} ${styles.customCardSelected}`}>
+                <div className="row">
+                    <div className={"d-none d-sm-block col-sm-6"}>
+                        <Image alt="card" src="/images/favicon.png" width={50} height={50}/>
+                    </div>
+                    {canClose && <div className="col-12 col-sm-6 text-end">
+                        <button className="btn btn-sm btn-danger rounded-5 float-end t"
+                            >
+                            x
+                        </button>
+                    </div>}
+                </div>
 
-        </div>
+                <div className='text-center'>
+                    {value && <span className={`fw-bold text-primary ${styles.valueCard}`}>{value}</span>}
+                    {color && <MdAccountCircle color={color} size={60}/>}
+                </div>
+                <div className={`d-none d-sm-block ${styles.logoBottom}`}>
+                    <Image alt="card" src="/images/favicon.png" width={50} height={50}/>
+                </div>
+            </div>
+            {name &&
+                <div className="text-sm-center">
+                    <h3>{name}</h3>
+                </div>
+            }
+        {
+            badgeConfig && <OverlayTrigger trigger="click" placement={badgeConfig?.position} overlay={popover}>
+                <Badge bg={'danger'} pill={true}
+                       className={ badgeConfig.position == "left" ? "position-absolute top-0 start-0 translate-middle" : "position-absolute top-0 start-100 translate-middle"}>{badgeConfig.badgeText}</Badge>
+            </OverlayTrigger>
+        }
+    </div>
   )
 }
 
 const style = {
-  fontSize: '150px',
-  color: 'blue'
-  // backgroundColor: 'white'
+    fontSize: '150px',
+    color: 'blue'
+    // backgroundColor: 'white'
 }
 
 const backgoundNotSelectedStyle = {
-  backgroundImage: "url('/images/card-backgound.png')",
-  backgroundPosition: 'center',
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover',
-  width: '185px'
+    backgroundImage: "url('/images/card-backgound.png')",
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    width: '185px'
 }
 
 const backgoundSelectedStyle = {
-  backgroundColor: 'yellow',
-  width: '185px'
+    backgroundColor: 'yellow',
+    width: '185px'
 }
 export default Card
--- components/Deck.tsx
+++ components/Deck.tsx
@@ -3,15 +3,22 @@ import Card from "./Card";
 
 export interface DeckProps {
   deck: [];
+  updateSelection: Function;
 }
-export const Deck = ({ deck }: DeckProps) => {
+export const Deck = ({ deck, updateSelection }: DeckProps) => {
 const [selectedIndex, setSelectedIndex] = useState(-1);
+
+const selectIndex = ( index: number) => {
+    console.log('deck ' + index);
+    setSelectedIndex(index);
+    updateSelection(deck[index])
+}
 
   return (
     <div className="container">
       <div className="row">
         {deck.map((score, index) => (
-          <div className="col-4" key={score} onClick={() => setSelectedIndex(index)}>
+          <div className="col" key={score} onClick={() => selectIndex(index)}>
             <Card key={score} value={score} canClose={false} selected={selectedIndex === index} />
           </div>
         ))}
