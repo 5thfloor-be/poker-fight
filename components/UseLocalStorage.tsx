265633feb3e5d00df9c85766dc5e4b5f9c525ede
michaeldupuy <contact@michaeldupuy.fr>
2022-09-20 13:24:25 +0200

    Commit UseLocalStorage

--- components/UseLocalStorage.tsx
+++ components/UseLocalStorage.tsx
@@ -1,29 +1,29 @@
 import { useState, useEffect } from "react";
 
-export type Vars = "USERNAME" | "COLOR";
+export type Vars = "USERNAME" | "COLOR" | "USER";
 
 export function getStorageValue(key: Vars, defaultValue: any) {
-    if (typeof window !== "undefined") {
-        const saved = localStorage.getItem(key);
-        const initial = saved ? JSON.parse(saved) : null;
-        return initial || defaultValue;
-    }
+  if (typeof window !== "undefined") {
+    const saved = localStorage.getItem(key);
+    const initial = saved ? JSON.parse(saved) : null;
+    return initial || defaultValue;
+  }
 }
 
 export function setStorageValue(key: Vars, value: any) {
-    if (typeof window !== "undefined") {
-        localStorage.setItem(key, JSON.stringify(value));
-    }
+  if (typeof window !== "undefined") {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
 }
 
 export const useLocalStorage = (key: Vars, defaultValue: any) => {
-    const [value, setValue] = useState(() => {
-        return getStorageValue(key, defaultValue);
-    });
+  const [value, setValue] = useState(() => {
+    return getStorageValue(key, defaultValue);
+  });
 
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value));
-    }, [key, value]);
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
 
-    return [value, setValue];
+  return [value, setValue];
 };
