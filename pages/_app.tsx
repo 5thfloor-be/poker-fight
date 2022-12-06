320daab1b29c565db2a83ac431a9adfe1c389a36
Gabriel Kaplan <gabriel.kaplan@g-kit.be>
2022-12-06 16:20:11 +0100

    fix matomo on all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,10 @@ import Layout from "../components/layout/Layout";
 import UserContextProvider from "../context/UserContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
+    if (typeof window !== "undefined") {
+        matomo();
+    }
+
   return (
     <UserContextProvider>
       <Layout>
@@ -15,3 +19,27 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
+export function matomo() {
+    if (document.getElementById("analytics") === null) {
+        const matomoUrl = process.env.NEXT_PUBLIC_MATOMO_URL;
+        const matomoSiteId = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+        console.debug('matomoUrl :', matomoUrl);
+        console.debug('matomoSiteId :', matomoSiteId);
+        var _paq = (window._paq = window?._paq || []);
+        _paq.push(["trackPageView"]);
+        _paq.push(["enableLinkTracking"]);
+        (function () {
+            var u = "//" + matomoUrl + "/";
+            _paq.push(["setTrackerUrl", u + "matomo.php"]);
+            _paq.push(["setSiteId", matomoSiteId]);
+            var d = document,
+                g = d.createElement("script"),
+                s = d.getElementsByTagName("script")[0];
+            g.async = true;
+            g.src = u + "matomo.js";
+            g.id = "analytics";
+            s?.parentNode?.insertBefore(g, s);
+        })();
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -32,10 +32,6 @@ const Home = () => {
     setShowLoading(true);
   };
 
-  if (typeof window !== "undefined") {
-    matomo();
-  }
-
   return (
     <div className="text-white">
       <Head>
@@ -377,26 +373,4 @@ export async function getServerSideProps() {
       //socket: socket
     },
   };
-}
-
-export function matomo() {
-  if (document.getElementById("analytics") === null) {
-    const matomoUrl = process.env.NEXT_PUBLIC_MATOMO_URL;
-    const matomoSiteId = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
-    var _paq = (window._paq = window._paq || []);
-    _paq.push(["trackPageView"]);
-    _paq.push(["enableLinkTracking"]);
-    (function () {
-      var u = "//" + matomoUrl + "/";
-      _paq.push(["setTrackerUrl", u + "matomo.php"]);
-      _paq.push(["setSiteId", matomoSiteId]);
-      var d = document,
-          g = d.createElement("script"),
-          s = d.getElementsByTagName("script")[0];
-      g.async = true;
-      g.src = u + "matomo.js";
-      g.id = "analytics";
-      s.parentNode.insertBefore(g, s);
-    })();
-  }
 }
