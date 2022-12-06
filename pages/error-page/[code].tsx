24ef4a8e74c34f5d2293a410f3fbcabed2befed6
Gabriel Kaplan <gabriel.kaplan@g-kit.be>
2022-12-06 18:33:39 +0100

    add config for matomo idsite

--- pages/error-page/[code].tsx
+++ pages/error-page/[code].tsx
@@ -14,7 +14,6 @@ const ErrorPage = () => {
   if (typeof window !== "undefined") {
     matomo("analyticsError");
   }
-
   const router = useRouter();
   const errorCode = router.query.code as string;
   const { setIsRoomActive, user, setUser } = useContext(UserContext);
