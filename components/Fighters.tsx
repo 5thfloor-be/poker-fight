2cf0fd0ddabc0f10e1d9c19f5d7d796f21d04b9d
michaeldupuy <contact@michaeldupuy.fr>
2022-12-09 14:16:47 +0100

    Improvements VS Room

--- components/Fighters.tsx
+++ components/Fighters.tsx
@@ -1,13 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
 import versus from "../public/images/versus.webp";
-import fighter1 from "../public/images/versus/1.webp";
-import fighter2 from "../public/images/versus/2.webp";
-import fighter3 from "../public/images/versus/3.webp";
-import fighter4 from "../public/images/versus/4.webp";
-import fighter5 from "../public/images/versus/5.webp";
-import fighter6 from "../public/images/versus/6.webp";
-import fighter7 from "../public/images/versus/7.webp";
-import fighter8 from "../public/images/versus/8.webp";
 import Image, { StaticImageData } from "next/image";
 import { UserContext } from "../context/UserContext";
 import styles from "../styles/Versus.module.css";
@@ -22,18 +14,18 @@ const Fighters = () => {
 
   const [otherVoters, setOtherVoters] = useState<UserVote[]>();
 
-  const [fighterMath1, setFighterMath1] = useState<StaticImageData>();
-  const [fighterMath2, setFighterMath2] = useState<StaticImageData>();
+  const [fighterMath1, setFighterMath1] = useState<string>();
+  const [fighterMath2, setFighterMath2] = useState<string>();
 
-  const fightersImages = [
-    fighter1,
-    fighter2,
-    fighter3,
-    fighter4,
-    fighter5,
-    fighter6,
-    fighter7,
-    fighter8,
+  const fightersImages: string[] = [
+    "../public/images/versus/1.webp",
+    "../public/images/versus/2.webp",
+    "../public/images/versus/3.webp",
+    "../public/images/versus/4.webp",
+    "../public/images/versus/5.webp",
+    "../public/images/versus/6.webp",
+    "../public/images/versus/7.webp",
+    "../public/images/versus/8.webp",
   ];
 
   useEffect(() => {
